6b90364981dca4b9949d056edaab44306a028e21
NaNdalal-dev <dnandalal7@gmail.com>
2021-06-22 22:29:04 +0530

    Added Files 2

--- TinyUrls/TinyUrls/urls.py
+++ TinyUrls/TinyUrls/urls.py
@@ -1,7 +1,14 @@
-from django.contrib import admin
-from django.urls import path
-from .views import*
+from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.urls import path, include
+from .views import*
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home)
