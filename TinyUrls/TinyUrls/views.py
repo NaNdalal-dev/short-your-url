7e7aa98d95de43959d5fa39e48072aeaae88718c
NaNdalal-dev <dnandalal7@gmail.com>
2021-06-22 20:37:31 +0530

    Added files

--- TinyUrls/TinyUrls/views.py
+++ TinyUrls/TinyUrls/views.py
@@ -5,14 +5,19 @@ from pyshorteners.exceptions import BadURLException
 def home(request):
 	tiny_url = None
 	error = None
+	original_url = None
 	if request.POST:
 		try:
+			original_url = request.POST['url']
 			url = Shortener()	
-			tiny_url = url.tinyurl.short(request.POST['url'])
+			tiny_url = url.tinyurl.short(original_url)
 			
 		except BadURLException:
 			error = "Invalid URL."
 		except:
 			error = "Oops!! Something Went wrong."
 
-	return render(request,'main.html',{'url':tiny_url,'error':error})
+	return render(request,'main.html',{
+		'url':tiny_url,
+		'error':error,
+		'original_url':original_url})
